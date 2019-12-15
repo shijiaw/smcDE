f81cdd7acf5579c83cb516effb33edb006952074
shijiaw <shijiaw@sfu.ca>
2019-12-14 19:08:58 -0800

    no message

--- Demo/Demo2/Demo2.R
+++ Demo/Demo2/Demo2.R
@@ -12,7 +12,7 @@ load("data_DDE.Rdata")
 load("time.Rdata")
 
 # run SMC
-output = smcDE(data = data_DDE, times = time, seed = 364, knots = seq(time[[1]][1], max(time[[1]]),by = 5), CESSthresholds = 0.8, NP = 500, resampleThreshold = 0.5, alambda = 1, blambda = 1, sigmac = 100, DEmodel = 2)
+output = smcDE(data = data_DDE, times = time, seed = 364, knots = seq(time[[1]][1], max(time[[1]]),by = 2), CESSthresholds = 0.8, NP = 300, resampleThreshold = 0.5, alambda = 1, blambda = 1, sigmac = 100, DEmodel = 2)
 
 
 
